273471bc7c1c05e0f776ec7d9a28f2f09cdedc4d
saoirse-defi <saoirsef@yahoo.co.uk>
2021-05-18 13:31:22 +0000

    add correct template for render

--- website/view.py
+++ website/view.py
@@ -1,8 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 
 view = Blueprint('view', __name__)
 
 
 @view.route('/')
 def home():
-    return "<h1>CUNT!</h1>"
+    return render_template('home.html')
