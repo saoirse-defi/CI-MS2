2408528ff673f17a1290c7390f09cf2e79192b4a
saoirse-defi <saoirsef@yahoo.co.uk>
2021-05-18 13:30:31 +0000

    add login, signup templates

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login') 
 def login():
-    return "<p>Logged in you cunt!</p>"
+    return render_template("login.html")
+
+
+@auth.route('/signup')
+def signup():
+    return render_template("signup.html")
