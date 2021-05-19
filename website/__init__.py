a3ba908344a9e2594d558b073cbde45eb237eacc
saoirse-defi <saoirsef@yahoo.co.uk>
2021-05-19 15:54:48 +0000

    add flask alchemy db setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "profiles.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'LFHLKNGRDKGLNSGLKGN'
+    app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .view import view
     from .auth import auth
