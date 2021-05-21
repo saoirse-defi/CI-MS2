45fe60048bb9283a118b7ab607d189b2e9e366d7
saoirse-defi <saoirsef@yahoo.co.uk>
2021-05-21 13:25:02 +0000

    set IP address and port number for localhost

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 from website import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.environ.get("IP", "0.0.0.0"), 
+            port=int(os.environ.get("PORT", "5000")), 
+            debug=True)
 
       
