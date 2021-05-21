ab6fd9b0adcd446f9ab21bd33ea2009cf36940cd
saoirse-defi <saoirsef@yahoo.co.uk>
2021-05-21 13:27:18 +0000

    add etherscan_transactions function for api call

--- website/etherscan_api.py
+++ website/etherscan_api.py
@@ -1,5 +1,20 @@
-import requests 
+import requests
 
+
+def etherscan_transactions(apikey, address):
+    url = "https://api.etherscan.io/api?module=account&action=txlist&address="\
+            + address + \
+            "&startblock=0&endblock=99999999&sort=asc&apikey=" + API_KEY
+    response = requests.get(url)
+    response_json = response.json()
+    transactions = response_json.get("result")
+    
+    for transaction in transactions:
+        print(transaction)
+
+
+etherscan_transactions("PQWGH496A8A1H3YV5TKWNVCPHJZ3S7ITHA", 
+                       "0x2158bC4E35eC077cB2Db80ea283683d0dF4C9A24")
 # API key for etherscan
 API_KEY = "PQWGH496A8A1H3YV5TKWNVCPHJZ3S7ITHA"  
 
