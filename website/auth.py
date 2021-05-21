904d558399413649724e7a8b53b5095d8c26c6f5
saoirse-defi <saoirsef@yahoo.co.uk>
2021-05-21 13:26:58 +0000

    add redirect upon logout

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, logout_user, current_user, login_required
 
 auth = Blueprint('auth', __name__)
 
@@ -16,17 +17,21 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again!', category='error')
         else:
             flash('Email is not recognised!', category='error')
 
-    return render_template("login.html", boolean=True)
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h1>Logged out</h1>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
@@ -34,6 +39,7 @@ def signup():
     if request.method == 'POST':
         email = request.form.get('email')
         nickname = request.form.get('nickname')
+        eth = request.form.get('eth')
         password = request.form.get('password')
         _password = request.form.get('password-confirm')
 
@@ -44,18 +50,22 @@ def signup():
             flash('Email Address is too short', category='error')
         elif len(nickname) < 2:
             flash('Your nickname is too short', category='error')
+        elif len(eth) < 40:
+            flash('The format of your Ethereum public address is incorrect',
+                  category='error')
         elif password != _password:
             flash('Your passwords dont match', category='error')
         elif len(password) < 8:
             flash('Your password is too short', category='error')
         else:
             new_user = User(
-                email=email, 
-                nickname=nickname, 
+                email=email,
+                nickname=nickname,
+                eth=eth,
                 password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Sign Up Successful!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("signup.html", boolean=True)
+    return render_template("signup.html", user=current_user)
