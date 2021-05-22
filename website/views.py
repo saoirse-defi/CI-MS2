4750b35ca6e1a82586dafb2324ef4a19f10c9bbc
saoirse-defi <saoirsef@yahoo.co.uk>
2021-05-22 14:46:17 +0000

    add python variable to home view

--- website/views.py
+++ website/views.py
@@ -1,11 +1,27 @@
 from flask import Blueprint, render_template
 from flask_login import current_user, login_required
+from website import coinmarketcap_api
+from website import cmp_api
 
+#  pull in api data and display as a 2 decimal float
+bitcoin_price = "{:.2f}".format(coinmarketcap_api.get_coin_price('BTC'))
+ethereum_price = "{:.2f}".format(coinmarketcap_api.get_coin_price('ETH'))
+litecoin_price = "{:.2f}".format(coinmarketcap_api.get_coin_price('LTC'))
+result_arr = cmp_api.get_price_data()
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
+@views.route('/home')
 @login_required
 def home():
-    return render_template('home.html', user=current_user)
+    return render_template('home.html',
+                           user=current_user, 
+                           bitcoin_price=bitcoin_price, 
+                           ethereum_price=ethereum_price,
+                           litecoin_price=litecoin_price,
+                           result_arr=result_arr)
+
+
+
