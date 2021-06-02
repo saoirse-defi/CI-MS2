87d65c955952db205203bb70339d5a0407f6d411
saoirse-defi <saoirsef@yahoo.co.uk>
2021-06-02 10:54:56 +0000

    add DOMcententLoaded event listener

--- website/static/app.js
+++ website/static/app.js
@@ -1,15 +1,37 @@
-/*const ethAddress = document.getElementById('ethAddress');
+const ethAddress = document.getElementById('ethAddress');
+let user;
+let userAddress;
 
 Moralis.initialize("ODLoFKUVQy2afCMmNL784vXb2DgVvjxntguF4x3c");
 Moralis.serverURL = "https://btjfheuh8ncx.moralis.io:2053/server";
 
 async function init(){
     window.web3 = await Moralis.Web3.enable();
+    login();
 }
 
-async function login() {
-    let user = Moralis.User.current();
+async function login(){
+    try{
+        user = Moralis.User.current();
+        if (!user) {
+            user = await Moralis.Web3.authenticate(); //prompts user to sign transcation with Metamask
+        }
+        getAddress(user);
 
+        userAddress = user['attributes'].accounts[0]; //location of ethereum address within the ParseUser object
+    
+        ethAddress.innerText = userAddress; //displays address to the user
+
+        document.getElementById("btn-login").style.display = "none";
+
+        console.log("Button Pressed!");
+    }catch(error){
+        console.log(error)
+    }
+}
+
+/*async function login() {
+    
     if (!user) {
         user = await Moralis.Web3.authenticate(); //prompts user to sign transcation with Metamask
     }
@@ -19,17 +41,25 @@ async function login() {
     userAddress = user['attributes'].accounts[0]; //location of ethereum address within the ParseUser object
     
     ethAddress.innerText = userAddress; //displays address to the user
-}
 
-async function getAddress(user){
+    document.getElementById("btn-login").style.display = "none";
+
+    console.log("Button Pressed!");
+}*/
+
+ async function getAddress(user){
     if (user) {
         const query = new Moralis.Query("EthTransactions"); //create a new eth transactions query using Moralis
         query.equalTo("from_address", user.get("ethAddress")); 
+        console.log(query);
 
         const results = await query.find(); //results of query
         console.log("user transactions:", results);
     }
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    init();
+});
 
-document.getElementById("btn-login").onclick = login();*/ 
+document.getElementById("btn-login").onclick = login();
