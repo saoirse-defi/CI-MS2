1262484753f1cbec7862999d333d9010caa7fc76
saoirse-defi <saoirsef@yahoo.co.uk>
2021-06-02 10:56:51 +0000

    add transaction_list array to display transactions

--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import requests
 from flask_login import current_user, login_required
 from website import cmp_api
 from website import webscrap
+from website import etherscan_api
+from website import models
 
+address = "0x6BF65C8278674FE0F6EF847c3eea95f3b8481178"
+
+transaction_list = etherscan_api.etherscan_transactions(address)
 
 price_dict = cmp_api.get_price_data()
 
@@ -18,12 +24,13 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/')
-@views.route('/home')
+@views.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
     return render_template('home.html',
                            user=current_user,
-                           price_dict=price_dict)
+                           price_dict=price_dict,
+                           transaction_list=transaction_list)
 
 
 @views.route('/news')
