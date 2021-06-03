8e4ba5bf9bda871e637abc9cbdb8358f9be3b93c
saoirse-defi <saoirsef@yahoo.co.uk>
2021-06-03 13:07:13 +0000

    add shorten, toInt, threeDecimals methods to home template

--- website/views.py
+++ website/views.py
@@ -5,11 +5,6 @@ from website import webscrap
 from website import etherscan_api
 from website import models
 
-address = "0x6BF65C8278674FE0F6EF847c3eea95f3b8481178"
-
-transaction_list = etherscan_api.etherscan_transactions(address)
-
-transaction_table_headings = ['Date created', 'From', 'To', 'Value', 'Gas Price', 'Gas Spent']
 
 price_dict = cmp_api.get_price_data()
 
@@ -28,11 +23,28 @@ views = Blueprint('views', __name__)
 @views.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
+    address = "0x6BF65C8278674FE0F6EF847c3eea95f3b8481178"
+    transaction_list = etherscan_api.etherscan_transactions(address)
+    transaction_table_headings = ['Date created', 'To', 'From', 'Value[ETH]', 'Gas Price[Gwei]', 'Gas Spent[ETH]']
+
+    def shorten(string):
+        return "0x..." + string[38:]
+
+    def toInt(x):
+        return int(float(x))
+
+    def threeDecimals(y):
+        return "%.3f" % y
+
     return render_template('home.html',
                            user=current_user,
                            price_dict=price_dict,
                            transaction_list=transaction_list,
-                           transaction_table_headings=transaction_table_headings)
+                           transaction_table_headings=transaction_table_headings,
+                           address=address,
+                           shorten=shorten,
+                           toInt=toInt,
+                           threeDecimals=threeDecimals)
 
 
 @views.route('/news')
