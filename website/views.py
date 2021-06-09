c6ec54adbd097acece86332b32b07d4eee18f993
saoirse-defi <saoirsef@yahoo.co.uk>
2021-06-09 18:02:56 +0000

    create separate transaction view

--- website/views.py
+++ website/views.py
@@ -5,6 +5,8 @@ from website import webscrap
 from website import etherscan_api
 from website import models
 from website import mediastack
+from datetime import datetime
+from dateutil import parser
 
 
 price_dict = cmp_api.get_price_data()
@@ -27,7 +29,7 @@ def home():
     address = "0x6BF65C8278674FE0F6EF847c3eea95f3b8481178"
     transaction_list = etherscan_api.etherscan_transactions(address)
     erc20_transaction_list = etherscan_api.erc20_transactions(address)
-    transaction_table_headings = ['Date created', 'Hash', 'To', 'From', 'Value', 'Token Involved', 'Gas Price', 'Gas Spent']
+
     gas_price_dict = etherscan_api.etherscan_gas()
     gas_total = etherscan_api.find_total_gas_spent(transaction_list)
     fav_coins = etherscan_api.find_fav_coins(erc20_transaction_list)
@@ -36,13 +38,8 @@ def home():
     highest_gas_erc20 = etherscan_api.find_highest_gas(erc20_transaction_list)
     highest_gas = int(max(highest_gas_eth, highest_gas_erc20))
 
-    bitcoin_stream = mediastack.mediastack_scrape('bitcoin')
-
-    def shorten(string):
-        return "0x..." + string[38:]
-    
-    def shorten2(string):
-        return "0x..." + string[62:]
+    average_gas_ethereum = etherscan_api.find_average_gas(transaction_list)
+    average_gas_erc20 = etherscan_api.find_average_gas(erc20_transaction_list)
 
     def toInt(x):
         return int(float(x))
@@ -53,19 +50,13 @@ def home():
     return render_template('home.html',
                            user=current_user,
                            price_dict=price_dict,
-                           transaction_list=transaction_list,
-                           transaction_table_headings=transaction_table_headings,
-                           address=address,
-                           shorten=shorten,
-                           shorten2=shorten2,
-                           toInt=toInt,
-                           threeDecimals=threeDecimals,
                            gas_price_dict=gas_price_dict,
                            gas_total=gas_total,
                            erc20_transaction_list=erc20_transaction_list,
                            fav_coins=fav_coins,
                            highest_gas=highest_gas,
-                           stream=stream)
+                           average_gas_ethereum=average_gas_ethereum,
+                           average_gas_erc20=average_gas_erc20)
 
 
 @views.route('/news')
@@ -78,3 +69,48 @@ def news():
 
 
 
+@views.route('/news2')
+def news2():
+    bitcoin_stream = mediastack.mediastack_scrape('bitcoin')
+    ethereum_stream = mediastack.mediastack_scrape('ethereum')
+    altcoin_stream = mediastack.altcoin_news()
+    current_time = parser.parse(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    return render_template('news2.html',
+                           user=current_user,
+                           bitcoin_stream=bitcoin_stream,
+                           ethereum_stream=ethereum_stream,
+                           altcoin_stream=altcoin_stream,
+                           current_time=current_time)
+
+
+@views.route('/transactions')
+def transactions():
+    address = "0x6BF65C8278674FE0F6EF847c3eea95f3b8481178"
+    transaction_list = etherscan_api.etherscan_transactions(address)
+    erc20_transaction_list = etherscan_api.erc20_transactions(address)
+    transaction_table_headings = ['Date created', 'Hash', 'To', 'From', 'Value', 'Token Involved', 'Gas Price', 'Gas Spent']
+    
+    def shorten(string):
+        return "0x..." + string[38:]
+    
+    def shorten2(string):
+        return "0x..." + string[62:]
+
+    def toInt(x):
+        return int(float(x))
+
+    def threeDecimals(y):
+        return "%.3f" % y
+
+    return render_template('transactions.html',
+                           user=current_user,
+                           transaction_list=transaction_list,
+                           erc20_transaction_list=erc20_transaction_list,
+                           transaction_table_headings=transaction_table_headings,
+                           address=address,
+                           shorten=shorten,
+                           shorten2=shorten2,
+                           toInt=toInt,
+                           threeDecimals=threeDecimals
+                           )
